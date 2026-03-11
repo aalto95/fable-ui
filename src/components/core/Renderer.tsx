adcba0c5321734a41d3a15f8e22593515479e19a
aalto95 <43157835+aalto95@users.noreply.github.com>
2026-03-11 23:27:04 +0900

    feat: initial commit

---
+++ src/components/core/Renderer.tsx
@@ -0,0 +1,11 @@
+import type React from "react";
+import { Button } from "../ui/button";
+
+export const Renderer: React.FC = () => {
+  return (
+    <>
+      <Button>Hello</Button>
+      <Button>Hello</Button>
+    </>
+  );
+};
