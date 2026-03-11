3731e5f4fe228096363bc9fd35dd055de24aab14
aalto95 <43157835+aalto95@users.noreply.github.com>
2026-03-11 23:29:43 +0900

    Start mocking up schema

--- src/components/core/Renderer.tsx
+++ src/components/core/Renderer.tsx
@@ -1,11 +1,31 @@
 import type React from "react";
+import { useState } from "react";
 import { Button } from "../ui/button";
 
+interface Schema {
+  ui: {
+    type: string;
+    children: {
+      type: string;
+      text: string;
+    }[];
+  }[];
+}
+
 export const Renderer: React.FC = () => {
+  const [schema, setSchema] = useState<Schema>();
+
+  fetch("./ui-schema.json").then((resp) => {
+    resp.json().then((jsonSchema) => {
+      setSchema(jsonSchema);
+    });
+  });
+
   return (
     <>
-      <Button>Hello</Button>
-      <Button>Hello</Button>
+      {schema?.ui.map((el) => {
+        return el.type;
+      })}
     </>
   );
 };
